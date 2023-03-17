223f71832ad140f97d5b5098aba044a59cd1ab72
ethxn08 <ethanalfaro08@gmail.com>
2023-03-17 11:18:10 +0100

    Socket Logic

--- models/sockets.js
+++ models/sockets.js
@@ -1,6 +1,11 @@
+const TicketList = require("./ticket-list");
+
 class Sockets {
   constructor(io) {
     this.io = io;
+
+    //Instancia de TicketList
+    this.ticketList = new TicketList();
 
     this.socketEvents();
   }
@@ -8,11 +13,18 @@ class Sockets {
   socketEvents() {
     // On connection
     this.io.on("connection", (socket) => {
+      console.log("Cliente conectado");
       // Escuchar evento: mensaje-to-server
-      socket.on("mensaje-to-server", (data) => {
-        console.log(data);
+      socket.on("request-ticket", (data, callback) => {
+        const newTicket = this.ticketList.createTicket();
+        callback(newTicket);
+      });
 
-        this.io.emit("mensaje-from-server", data);
+      socket.on("next-ticket-works", ({ agent, desktop }, callback) => {
+        const yourTicket = this.ticketList.asignTicket(agent, desktop);
+        callback(yourTicket);
+
+        this.io.emit("asigned-ticket", this.ticketList.last13);
       });
     });
   }
